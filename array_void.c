fec049c62e04be301bd853accc56514df454ad52
Apurva <58025903+apurvagurme@users.noreply.github.com>
2020-05-10 11:12:03 +0530

    Removed unnecessary count variable

--- array_void.c
+++ array_void.c
@@ -32,11 +32,9 @@ Object increment_void(Object num)
 ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper)
 {
   ArrayVoid_ptr result = create_void_array(src->length);
-  int count = 0;
   for (int i = 0; i < src->length; i++)
   {
-    result->array[count] = mapper(src->array[i]);
-    count++;
+    result->array[i] = mapper(src->array[i]);
   }
   return result;
 }
